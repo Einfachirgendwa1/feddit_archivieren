4a7bd7e54d73135dc1e3651e9aa52c2690c11776
Einfachirgendwa1 <faris@karge.berlin>
2024-05-08 16:36:15 +0200

    Einfacher daemon

---
+++ .nvim-dap.lua
@@ -0,0 +1,20 @@
+local dap = require("dap")
+
+dap.adapters.lldb = {
+	type = "executable",
+	command = "/usr/bin/lldb-vscode",
+	name = "lldb",
+}
+
+dap.configurations.rust = {
+	{
+		name = "hello-world",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.getcwd() .. "/target/debug/feddit_archivieren"
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+	},
+}
