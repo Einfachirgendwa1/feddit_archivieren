6de5cc221d2a9ce4d3ee32f368b084300a8f6bed
Einfachirgendwa1 <faris@karge.berlin>
2024-05-10 21:14:20 +0200

    Zsh autocompletion TODO bei update_local entfernt

--- feddit_archivieren.zsh
+++ feddit_archivieren.zsh
@@ -1,4 +1,4 @@
 #compdef feddit_archivieren
 local -a subcmds
-subcmds=('start:Startet den Daemon' 'install:Installiert das Programm' 'kill:Killt den Daemon' 'update:Updated den Daemon' 'update_local:TODO:Updated den Daemon anhand des aktuellen Directorys' 'clean:Löscht alle vom Programm erstellten Dateien, außer das Programm selbst' 'info:Zeigt Informationen über den Daemon an')
+subcmds=('start:Startet den Daemon' 'install:Installiert das Programm' 'kill:Killt den Daemon' 'update:Updated den Daemon' 'update_local:Updated den Daemon anhand des aktuellen Directorys' 'clean:Löscht alle vom Programm erstellten Dateien, außer das Programm selbst' 'info:Zeigt Informationen über den Daemon an')
 _describe 'command' subcmds
