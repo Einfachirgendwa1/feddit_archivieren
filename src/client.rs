f38f664bf5bb7631330ac8447194a5b3fc7015ba
Einfachirgendwa1 <faris@karge.berlin>
2024-05-17 15:12:36 +0200

    Output von make clean install beim Update wird jetzt printet

--- src/client.rs
+++ src/client.rs
@@ -118,6 +118,8 @@ fn main() {
                 .output()
             {
                 Ok(output) => {
+                    println!("{:?}", output.stdout);
+                    println!("{:?}", output.stderr);
                     if !output.status.success() {
                         println!("Fehler bei der Installation.");
                         println!("{}", command_output_formater(&output));
