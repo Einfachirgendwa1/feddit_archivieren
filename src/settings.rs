91bf64322db320fed40eb741ae88a10f9c76638e
Einfachirgendwa1 <faris@karge.berlin>
2024-05-18 13:51:27 +0200

    Finally herausgefunden das sowas für eine Datei anstelle von für jedes item individuell geht

--- src/settings.rs
+++ src/settings.rs
@@ -1,23 +1,13 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+pub const DAEMON_PATH: &'static str = "/usr/bin/feddit_archive_daemon";
+pub const CLIENT_PATH: &'static str = "/usr/bin/feddit_archivieren";
+pub const RUN_DIR: &'static str = "/run/feddit_archivieren";
+pub const PID_FILE: &'static str = "/run/feddit_archivieren/daemon.pid";
+pub const ERR_FILE: &'static str = "/run/feddit_archivieren/daemon.err";
+pub const OUT_FILE: &'static str = "/run/feddit_archivieren/daemon.out";
+pub const SOCKET_FILE: &'static str = "/run/feddit_archivieren/daemon.sck";
+pub const UDPATE_DIR: &'static str = "/tmp/feddit_archivieren";
+pub const GITHUB_LINK: &'static str = "https://github.com/Einfachirgendwa1/feddit_archivieren";
+pub const TCP_BUFFER_SIZE: usize = 1024;
 pub const FEDDIT_LINK: &'static str =
     "https://feddit.de/?dataType=Post&listingType=Local&page=1&sort=New";
-#[allow(dead_code)]
-pub const DAEMON_PATH: &'static str = "/usr/bin/feddit_archive_daemon";
-#[allow(dead_code)]
-pub const CLIENT_PATH: &'static str = "/usr/bin/feddit_archivieren";
-#[allow(dead_code)]
-pub const RUN_DIR: &'static str = "/run/feddit_archivieren";
-#[allow(dead_code)]
-pub const PID_FILE: &'static str = "/run/feddit_archivieren/daemon.pid";
-#[allow(dead_code)]
-pub const ERR_FILE: &'static str = "/run/feddit_archivieren/daemon.err";
-#[allow(dead_code)]
-pub const OUT_FILE: &'static str = "/run/feddit_archivieren/daemon.out";
-#[allow(dead_code)]
-pub const SOCKET_FILE: &'static str = "/run/feddit_archivieren/daemon.sck";
-#[allow(dead_code)]
-pub const UDPATE_DIR: &'static str = "/tmp/feddit_archivieren";
-#[allow(dead_code)]
-pub const GITHUB_LINK: &'static str = "https://github.com/Einfachirgendwa1/feddit_archivieren";
-#[allow(dead_code)]
-pub const TCP_BUFFER_SIZE: usize = 1024;
